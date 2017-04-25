3713963bf55dfdded69ff5a7217a5f5eb4cd429d
Sorrel Brigman <sorrel.k@gmail.com>
2017-04-25 14:04:38 -0500

    get one keeper also works

--- client/app/controllers/keeperCtrl.js
+++ client/app/controllers/keeperCtrl.js
@@ -3,6 +3,7 @@ app.controller('KeeperCtrl', function(ZookeeperFact, $scope){
 const popPage = () =>{
   ZookeeperFact.getAll()
     .then((keepers) => {
+      console.log(keepers)
       $scope.keepers = keepers
       $scope.$apply()
     })
--- client/app/controllers/oneKeeperCtrl.js
+++ client/app/controllers/oneKeeperCtrl.js
@@ -1,10 +1,12 @@
-app.controller('OneKeeperCtrl', function($scope, $routeParams){
+app.controller('OneKeeperCtrl', function($scope, $routeParams, ZookeeperFact){
 
   let keeperId = $routeParams.id;
+  console.log("id", keeperId);
   const popPage = () =>{
     ZookeeperFact.getOne(keeperId)
       .then((keeper) => {
-        $scope.keeper = keeper.keeper
+        $scope.keeper = keeper
+        console.log(keeper)
         $scope.$apply()
       })
   }
