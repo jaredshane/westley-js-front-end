a537783f15689a3202235dfa5e6985460531a6f2
Sorrel Brigman <sorrel.k@gmail.com>
2017-04-25 10:54:21 -0500

    commented out unused caitlin files

--- client/app/controllers/animalEditCtrl.js
+++ client/app/controllers/animalEditCtrl.js
@@ -1,3 +1,14 @@
-app.controller('AnimalEditCtrl', function(){
+app.controller('AnimalEditCtrl', function($scope, AnimalFact, ZookeeperFact){
+
+const popPage = () =>{
+  AnimalFact.getOne()
+    .then((animal) => {
+      $scope.animal = animal.animal
+      $scope.$apply()
+    })
+}
+
+popPage()
+
 
 });
